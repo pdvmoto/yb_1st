ac018e2b83da9cda9b889081850283b16d4f1196
pdvbv macbook <piet.devisser@cheerful.com>
2023-08-18 22:27:00 +0200

    latest

---
+++ chk_clu.sh
@@ -0,0 +1,50 @@
+#!/bin/ksh
+
+
+while true 
+do
+
+  for node in node1 node2 node3 node4 node5 node6 node7
+  do
+
+    echo doing node $node  
+    echo $node:
+    docker exec $node  ps -ef  | grep database_host | cut -d= -f2  
+    docker exec -it $node yugabyted status | grep atus; sleep 5 ; echo .
+
+  done
+
+  echo for loop done, next.. 
+  sleep 5
+
+done 
+
+while true 
+do
+
+  echo node1:
+  docker exec node1  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node1 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node2:
+  docker exec node2  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node2 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node3:
+  docker exec node3  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node3 yugabyted status | grep atus; sleep 3 ; echo .
+
+  echo node4:
+  docker exec node4  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+  echo node5:
+  docker exec node5  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+  echo node6:
+  docker exec node6  ps -ef  | grep database_host | cut -d= -f2  
+  docker exec -it node4 yugabyted status | grep atus; sleep 6 ; echo .
+
+done 
+
---
+++ chk_status.sh
@@ -0,0 +1,9 @@
+#!/bin/ksh
+
+docker exec -it node1 yugabyted status 
+docker exec -it node2 yugabyted status 
+docker exec -it node3 yugabyted status 
+docker exec -it node4 yugabyted status 
+docker exec -it node5 yugabyted status 
+docker exec -it node6 yugabyted status 
+
