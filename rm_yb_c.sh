47bba402c21fe0882c3f9b288d02240304797607
pdvbv macbook <piet.devisser@cheerful.com>
2023-08-21 11:29:36 +0200

    ore

---
+++ rm_yb_c.sh
@@ -0,0 +1,34 @@
+#!/bin/ksh
+#
+# rm_yb_c.sh: remove nodes 1-6
+#
+# This time: same 6 nodes, but more ports exposed.
+#
+# Q: is there a way to measure "effort", possibly "chattyness" ?
+#
+
+for node in node6 node5 node4 node3 node2 node1
+do
+  docker stop $node
+  sleep 2
+  docker rm $node
+  sleep 2
+done
+
+
+docker network rm yb_net
+
+sleep 2
+
+docker network list
+
+docker ps -a
+
+echo .
+echo rmoved, now verify: 
+echo  - docker ps -a
+echo  - docker network list 
+echo . 
+echo Have Fun.
+echo .
+echo .
