bf8288a218f2438f219b9381d6e56fbede3dafe0
pdvmoto <piet@basetide.com>
2024-01-29 14:53:21 +0100

    loop

--- mk_yi.sh
+++ mk_yi.sh
@@ -42,104 +42,90 @@ nodelist="node2 node3 node4"
 # docker network rm yb_net
 # sleep 2
 docker network create --subnet=172.20.0.0/16 --ip-range=172.20.0.0/24  yb_net
+echo .
+echo network yb_net created, next loop over nodes
+echo .
 sleep 2
 
-# start 1st master, call it node2, network address: node2.yb_net
-docker run -d --network yb_net  \
-  --hostname node2 --name node2 \
-  -p15432:15433 -p5432:5433     \
-  -p7002:7000 -p9002:9000       \
-  -v /Users/pdvbv/yb_data/node2:/root/var  \
-  $YB_IMAGE                             \
-  tail -f /dev/null
+# 
+# test: 
+#  - ways to start
+#  - masters ? 
+#  - with or without volume-mapping
+#  - how to get to K8s ??
+#
 
-#  tail -f /dev/null
+nodenrs="2 3 4 5 6 7 8"
+
+# create nodes, platform, no db yet...
+for nodenr in $nodenrs
+do
+
+  # define all relevant pieces (no spaces!)
+  hname=node${nodenr} 
+  pgport=543${nodenr}
+  yb7port=700${nodenr}
+  yb9port=900${nodenr}
+  yb15port=1543${nodenr}
+
+  echo creating node ${hname}
+
+  crenode=` \
+  echo docker run -d --network yb_net        \
+    --hostname $hname --name $hname          \
+    -p${pgport}:5433                         \
+    -p${yb7port}:7000 -p${yb9port}:9000      \
+    -p${yb15port}:15433                      \
+    -v /Users/pdvbv/yb_data/$hname:/root/var \
+    $YB_IMAGE                                \
+    tail -f /dev/null `
+ 
+  echo $crenode
+
+  # do it..
+  $crenode
+
+  echo .
+
+  sleep 1
+
+done
+
+
+echo .
+echo nodes created: 5 sec to Cntr-C .. or .. continue doing it slower forever...
+echo . 
 
 sleep 5
+
+
+echo .
+echo node2 is the first node, need to Create the DB, other will just Join
+echo .
 
 docker exec node2 yugabyted start --background=true --ui=true
 
-# found out the hard way that a small pause is beneficial
-sleep 5
-
-#now add nodes..
-docker run -d --network yb_net  \
-  --hostname node3 --name node3 \
-  -p15433:15433 -p5433:5433     \
-  -p7003:7000 -p9003:9000       \
-  -v /Users/pdvbv/yb_data/node3:/root/var  \
-  $YB_IMAGE                             \
-  tail -f /dev/null
-
-# yugabyted start --background=true --join=node2
-
-#  use this to start a server-node  tail -f /dev/null
-
-sleep 5
-
-docker exec node3 yugabyted start --background=true --join=node2
-
-sleep 5
-
-docker run -d --network yb_net  \
-  --hostname node4 --name node4 \
-  -p15434:15433 -p5434:5433     \
-  -p7004:7000 -p9004:9000       \
-  -v /Users/pdvbv/yb_data/node4:/root/var  \
-  $YB_IMAGE                             \
-  tail -f /dev/null
-
-# yugabyted start --background=true --join=node2
-
-# tail -f /dev/null
-
-sleep 5
-
-docker exec node4 yugabyted start --background=true --join=node2
-
-sleep 1
-
 echo .
-echo Now 3 nodes done as severs. use ENTER to Continue. Control C to stop...
+echo note: we tolerate an error for node2 to allow uniform command for all nodes.
 echo .
-read abc
 
-docker run -d --network yb_net  \
-  --hostname node5 --name node5 \
-  -p15435:15433 -p5435:5433     \
-  -p7005:7000 -p9005:9000       \
-  $YB_IMAGE                             \
-  yugabyted start --background=false --join node2
+sleep 5
 
-sleep 10
+for nodenr in $nodenrs
+do
 
-docker run -d --network yb_net  \
-  --hostname node6 --name node6 \
-  -p15436:15433 -p5436:5433     \
-  -p7006:7000 -p9006:9000       \
-  $YB_IMAGE                             \
-  yugabyted start --background=false --join node2
+  hname=node${nodenr} 
 
+  startcmd=`echo docker exec ${hname} yugabyted start --join=node2 `
 
-sleep 10
+  echo command will be : ${startcmd}
 
-docker run -d --network yb_net  \
-  --hostname node7 --name node7 \
-  -p15437:15433 -p5437:5433     \
-  -p7007:7000 -p9007:9000       \
-  $YB_IMAGE                             \
-  yugabyted start --background=false --join node2
+  ${startcmd}
 
+  echo .
+  sleep 5
 
-docker run -d --network yb_net  \
-  --hostname node8 --name node8 \
-  -p15438:15433 -p5438:5433     \
-   -p7008:7000  -p9008:9000     \
-  $YB_IMAGE                             \
-  yugabyted start --background=false --join node2
-
-
-#  -p13007:13000 -p12007:12000   \
+done
 
 
 # ---- add a generic platform, worker-node.. ----
