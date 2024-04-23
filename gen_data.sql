909203570981bbb2f3bd8b913cd60d45a1a19f45
pdvbv macbook <piet.devisser@cheerful.com>
2024-04-23 09:27:35 +0200

    gd

--- gen_data.sql
+++ gen_data.sql
@@ -1,12 +1,36 @@
+\timing on
 
---drop table if exists gen1 cascade;
+drop table if exists d1 cascade;
+drop table if exists d2 cascade;
 
-create table gen1 (
-  data text primary key
+\! sleep 2
+
+create table d1 (
+  data text  primary key
 );
 
-copy gen1 (data) from program
+\! sleep 2
+
+copy d1 (data) from program
 $bash$
-base64 -i /dev/urandom -w 100 | head -100000
-$bash$ with ( replace )
-\watch 1
+base64 -i /dev/urandom -w 1024 | head -100000
+$bash$ with ( replace ) ;
+
+\! sleep 2
+
+create table d2 (
+  id bigint generated always as identity primary key
+, data text  
+);
+
+-- cache so big it never hurts
+-- alter sequence d2_id_seq cache 1000000 ; 
+
+\! sleep 2
+
+copy d2 (data) from program
+$bash$
+base64 -i /dev/urandom -w 1024 | head -100000
+$bash$ with ( replace ) ;
+
+
