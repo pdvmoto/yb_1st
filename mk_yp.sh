6425bc81c99f91f05563010670b7742a58d3b298
pdvmoto <piet@basetide.com>
2024-09-15 17:00:27 +0200

    podman

--- mk_yp.sh
+++ mk_yp.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 # set -v -x
 #
-# mk_yi.sh: generate a yb cluster in docker, interactive nodes (servers).
+# mk_yi.sh: generate a yb cluster in Podman, interactive nodes (servers).
 #
 # This time: same 6+ nodes, same ports exposed.
 # trick: start with some other program to similate server-light containers, 
@@ -28,8 +28,8 @@
 # old image: 
 #  this one worked.. yugabytedb/yugabyte:2.19.0.0-b190        \
 # newer image check sites...
-# docker pull yugabytedb/yugabyte:2.19.3.0-b140
-# docker pull yugabytedb/yugabyte:2.20.1.0-b97      
+# podman pull yugabytedb/yugabyte:2.19.3.0-b140
+# podman pull yugabytedb/yugabyte:2.20.1.0-b97      
 #
 # notes on placement:
 #   3 zones, with 2 nodes each ?
@@ -46,21 +46,17 @@
 # YB_IMAGE=yugabytedb/yugabyte:2.21.0.0-b545
 # YB_IMAGE=yugabytedb/yugabyte:2.21.1.0-b271
 # YB_IMAGE=yugabytedb/yugabyte:2024.1.1.0-b137
-YB_IMAGE=abhinabsaha/yugabytedb:latest_with_pid
-
-# 26 Aug, didnt have yb_ash view ?
-# YB_IMAGE=yugabytedb/yugabyte:2.20.6.0-b66
 
 # latest was broken on 21-aug-2024?
-# YB_IMAGE=yugabytedb/yugabyte:latest
+YB_IMAGE=yugabytedb/yugabyte:latest
 
 # get some file to log stmnts, start simple
 LOGFILE=mk_nodes.log
 
 echo `date` $0 : creating cluster... >> $LOGFILE
 
-# docker network rm yb_net
-  docker network create --subnet=172.20.0.0/16 --ip-range=172.20.0.0/24  yb_net
+# podman network rm yb_net
+  podman network create --subnet=172.20.0.0/16 --ip-range=172.20.0.0/24  yb_net
 echo .
 echo network yb_net created, next loop over nodes
 echo .
@@ -74,7 +70,7 @@ sleep 2
 #  - how to get to K8s ??
 #
 
-  nodenrs="2 3 4 "
+  nodenrs="2 3 4"
 # nodenrs="  "
 
 echo `date` $0 : ---- creating cluster for nodes : $nodenrs -------
@@ -96,7 +92,7 @@ do
   echo .
 
   crenode=` \
-  echo docker run -d --network yb_net        \
+  echo podman run -d --network yb_net        \
     --hostname $hname --name $hname          \
     -p${pgport}:5433                         \
     -p${yb7port}:7000 -p${yb9port}:9000      \
@@ -122,49 +118,45 @@ do
   echo .
 
   echo $hname : adding profile to already present bashrc...
-  docker cp yb_profile.sh $hname:/tmp/
-  docker exec -it $hname sh -c "cat /tmp/yb_profile.sh >> /root/.bashrc "
+  podman cp yb_profile.sh $hname:/tmp/
+  podman exec -it $hname sh -c "cat /tmp/yb_profile.sh >> /root/.bashrc "
 
   echo $hname : adding psqlrc
-  docker cp ~/.psqlrc $hname:/tmp
-  docker exec -it $hname sh -c "cp /tmp/.psqlrc /root/.psqlrc"
+  podman cp ~/.psqlrc $hname:/tmp
+  podman exec -it $hname sh -c "cp /tmp/.psqlrc /root/.psqlrc"
 
   echo $hname : adding ybflags.conf
-  docker cp ybflags.conf $hname:/home/yugabyte/
+  podman cp ybflags.conf $hname:/home/yugabyte/
 
   echo $hname : adding psg ...
-  docker cp `which psg`     $hname:/usr/local/bin/psg
-  docker exec -it $hname chmod 755 /usr/local/bin/psg
+  podman cp `which psg`     $hname:/usr/local/bin/psg
+  podman exec -it $hname chmod 755 /usr/local/bin/psg
 
   echo $hname : adding ff ...
-  docker cp `which ff`      $hname:/usr/local/bin/ff
-  docker exec -it $hname chmod 755 /usr/local/bin/ff
+  podman cp `which ff`      $hname:/usr/local/bin/ff
+  podman exec -it $hname chmod 755 /usr/local/bin/ff
 
   echo $hname : adding do_ashloop.sh and st_ startscript ...
-  docker cp do_ashloop.sh             $hname:/usr/local/bin/do_ashloop.sh
-  docker exec -it $hname   chmod 755         /usr/local/bin/do_ashloop.sh
-  docker cp st_ashloop.sh             $hname:/usr/local/bin/st_ashloop.sh
-  docker exec -it $hname   chmod 755         /usr/local/bin/st_ashloop.sh
+  podman cp do_ashloop.sh             $hname:/usr/local/bin/do_ashloop.sh
+  podman exec -it $hname   chmod 755         /usr/local/bin/do_ashloop.sh
+  podman cp st_ashloop.sh             $hname:/usr/local/bin/st_ashloop.sh
+  podman exec -it $hname   chmod 755         /usr/local/bin/st_ashloop.sh
 
   echo $hname : add startsadc.sh or similar to help collect sar
-  docker cp startsadc.sh    $hname:/usr/local/bin/startsadc.sh
-  docker exec -it $hname chmod 755 /usr/local/bin/startsadc.sh
+  podman cp startsadc.sh    $hname:/usr/local/bin/startsadc.sh
+  podman exec -it $hname chmod 755 /usr/local/bin/startsadc.sh
   # detach, or do it later, bcse takes 30sec: 
-  # docker exec -it $hname startsadc.sh &
+  # podman exec -it $hname startsadc.sh &
   
   echo $hname : add do_stuff.sh or similar to help start all
-  docker cp do_stuff.sh     $hname:/usr/local/bin/do_stuff.sh
-  docker exec -it $hname chmod 755 /usr/local/bin/do_stuff.sh
-
-  echo $hname : add yb_boot.sh or similar to boot ybdb
-  docker cp yb_boot.sh      $hname:/usr/local/bin/yb_boot.sh
-  docker exec -it $hname chmod 755 /usr/local/bin/yb_boot.sh
+  podman cp do_stuff.sh    $hname:/usr/local/bin/do_stuff.sh
+  podman exec -it $hname chmod 755 /usr/local/bin/do_stuff.sh
 
   # more tooling... make sure the files are in working dir
 
   echo $hname : adding yugatool and enabling ysql_bench...
-  docker cp yugatool.gz $hname:/home/yugabyte/bin
-  cat <<EOF | docker exec -i $hname sh
+  podman cp yugatool.gz $hname:/home/yugabyte/bin
+  cat <<EOF | podman exec -i $hname sh
     gunzip /home/yugabyte/bin/yugatool.gz
     chmod 755 /home/yugabyte/bin/yugatool
     ln -s /home/yugabyte/bin/yugatool /usr/local/bin/yugatool
@@ -174,9 +166,9 @@ EOF
   # echo $hname : adding jq .... Why first 
   # skip jq, libs and yum need too much space ?
   echo $hname : installing jq and chrony ...
-  # docker cp jq $hname:/usr/bin/jq
-  docker exec $hname yum install jq -y
-  docker exec $hname yum install chrony -y
+  # podman cp jq $hname:/usr/bin/jq
+  podman exec $hname yum install jq -y
+  podman exec $hname yum install chrony -y
 
   echo .
   echo $hname : tools installed.
@@ -200,9 +192,9 @@ echo .
 echo node2 is the first node, need to Create the DB, other will just Join
 echo .
 
-# docker exec node2 yugabyted start --advertise_address=node2 --background=true --ui=true
+# podman exec node2 yugabyted start --advertise_address=node2 --background=true --ui=true
 
-  docker exec node2 yugabyted start \
+  podman exec node2 yugabyted start \
     --advertise_address=node2       \
     --tserver_flags=flagfile=/home/yugabyte/ybflags.conf \
      --master_flags=flagfile=/home/yugabyte/ybflags.conf 
@@ -220,7 +212,7 @@ do
 
   hname=node${nodenr} 
 
-  startcmd=`echo docker exec ${hname} yugabyted start --advertise_address=$hname --join=node2 \
+  startcmd=`echo podman exec ${hname} yugabyted start --advertise_address=$hname --join=node2 \
     --tserver_flags=flagfile=/home/yugabyte/ybflags.conf \
      --master_flags=flagfile=/home/yugabyte/ybflags.conf `
 
@@ -248,12 +240,12 @@ echo .
 
 # attempt at mulitple start-commands, to start sadc and ashloop in background, 
 # not working yet..
-# docker run -d --network yb_net --hostname nodeX --name nodeX yugabytedb/yugabyte:2.21.1.0-b271  sh -c " echo ` exec /usr/local/bin/do_stuff.sh ` > /var/log/start.log  && tail -f /dev/null" 
+# podman run -d --network yb_net --hostname nodeX --name nodeX yugabytedb/yugabyte:2.21.1.0-b271  sh -c " echo ` exec /usr/local/bin/do_stuff.sh ` > /var/log/start.log  && tail -f /dev/null" 
  
 hname=nodeX
 
 crenode=` \
-  echo docker run -d --network yb_net        \
+  echo podman run -d --network yb_net        \
     --hostname $hname --name $hname          \
     $YB_IMAGE  `
 
@@ -277,45 +269,45 @@ crenode=` \
   echo .
 
   echo $hname : adding profile to already present bashrc...
-  docker cp yb_profile.sh $hname:/tmp/
-  docker exec -it $hname sh -c "cat /tmp/yb_profile.sh >> /root/.bashrc "
+  podman cp yb_profile.sh $hname:/tmp/
+  podman exec -it $hname sh -c "cat /tmp/yb_profile.sh >> /root/.bashrc "
 
   echo $hname : adding psqlrc
-  docker cp ~/.psqlrc $hname:/tmp
-  docker exec -it $hname sh -c "cp /tmp/.psqlrc /root/.psqlrc"
+  podman cp ~/.psqlrc $hname:/tmp
+  podman exec -it $hname sh -c "cp /tmp/.psqlrc /root/.psqlrc"
 
   echo $hname : adding ybflags.conf
-  docker cp ybflags.conf $hname:/home/yugabyte/
+  podman cp ybflags.conf $hname:/home/yugabyte/
 
   echo $hname : adding psg ...
-  docker cp `which psg`     $hname:/usr/local/bin/psg
-  docker exec -it $hname chmod 755 /usr/local/bin/psg
+  podman cp `which psg`     $hname:/usr/local/bin/psg
+  podman exec -it $hname chmod 755 /usr/local/bin/psg
 
   echo $hname : adding ff ...
-  docker cp `which ff`      $hname:/usr/local/bin/ff
-  docker exec -it $hname chmod 755 /usr/local/bin/ff
+  podman cp `which ff`      $hname:/usr/local/bin/ff
+  podman exec -it $hname chmod 755 /usr/local/bin/ff
 
   echo $hname : adding do_ashloop.sh and st_ startscript ...
-  docker cp do_ashloop.sh             $hname:/usr/local/bin/do_ashloop.sh
-  docker exec -it $hname   chmod 755         /usr/local/bin/do_ashloop.sh
-  docker cp st_ashloop.sh             $hname:/usr/local/bin/st_ashloop.sh
-  docker exec -it $hname   chmod 755         /usr/local/bin/st_ashloop.sh
+  podman cp do_ashloop.sh             $hname:/usr/local/bin/do_ashloop.sh
+  podman exec -it $hname   chmod 755         /usr/local/bin/do_ashloop.sh
+  podman cp st_ashloop.sh             $hname:/usr/local/bin/st_ashloop.sh
+  podman exec -it $hname   chmod 755         /usr/local/bin/st_ashloop.sh
 
   echo $hname : add startsadc.sh or similar to help collect sar
-  docker cp startsadc.sh    $hname:/usr/local/bin/startsadc.sh
-  docker exec -it $hname chmod 755 /usr/local/bin/startsadc.sh
+  podman cp startsadc.sh    $hname:/usr/local/bin/startsadc.sh
+  podman exec -it $hname chmod 755 /usr/local/bin/startsadc.sh
   # detach, or do it later, bcse takes 30sec: 
-  # docker exec -it $hname startsadc.sh &
+  # podman exec -it $hname startsadc.sh &
   
   echo $hname : add do_stuff.sh or similar to help start all
-  docker cp do_stuff.sh    $hname:/usr/local/bin/do_stuff.sh
-  docker exec -it $hname chmod 755 /usr/local/bin/do_stuff.sh
+  podman cp do_stuff.sh    $hname:/usr/local/bin/do_stuff.sh
+  podman exec -it $hname chmod 755 /usr/local/bin/do_stuff.sh
 
   # more tooling... make sure the files are in working dir
 
   echo $hname : adding yugatool and enabling ysql_bench...
-  docker cp yugatool.gz $hname:/home/yugabyte/bin
-  cat <<EOF | docker exec -i $hname sh
+  podman cp yugatool.gz $hname:/home/yugabyte/bin
+  cat <<EOF | podman exec -i $hname sh
     gunzip /home/yugabyte/bin/yugatool.gz
     chmod 755 /home/yugabyte/bin/yugatool
     ln -s /home/yugabyte/bin/yugatool /usr/local/bin/yugatool
@@ -323,27 +315,27 @@ crenode=` \
 
 echo $0 : $hname created...
 
-# docker run -d --network yb_net  \
+# podman run -d --network yb_net  \
 #   --hostname nodeX --name nodeX \
 #   --ip 172.20.0.21              \
 #   $YB_IMAGE                             \
 #   yugabyted start --background=false --ui=true
 
 # use nodeY, to have a neutral node, 10 days idle, in the network, 
-# docker run -d --network yb_net  \
+# podman run -d --network yb_net  \
 #   --hostname nodeY --name nodeY \
 #   --ip 172.20.0.22              \
 #   $YB_IMAGE                     \
 #   sleep 999999 
 
 # health checks:
-# docker exec -it node2 yugabyted status 
-# docker exec -it node3 yugabyted status 
-# docker exec -it node4 yugabyted status 
-# docker exec -it node5 yugabyted status 
-# docker exec -it node6 yugabyted status 
-# docker exec -it node7 yugabyted status 
-# docker exec -it node8 yugabyted status 
+# podman exec -it node2 yugabyted status 
+# podman exec -it node3 yugabyted status 
+# podman exec -it node4 yugabyted status 
+# podman exec -it node5 yugabyted status 
+# podman exec -it node6 yugabyted status 
+# podman exec -it node7 yugabyted status 
+# podman exec -it node8 yugabyted status 
 
 echo .
 echo Scroll back and check if it all workd...
@@ -358,7 +350,7 @@ echo .    - run yb_init.sql and demo_fill.sql to load often-used functions.
 echo .    - run mk_ybash.sql prepare ash-logging
 echo .    - run mk_ashvws.sql to prepare live-ash viewing via gv$
 echo .    - use do_stuff.sh to run startsadc.sh and do_ashloop.sh
-echo .    - activate st/do_ahsloop.sh on every node,  why no nohup from docker exec? 
+echo .    - activate st/do_ahsloop.sh on every node,  why no nohup from podman exec? 
 echo .    - activate startsadc.sh to use sar
 echo .    - run demo_fill.sql to load demo-table t, use checks/monitor.
 echo .    - run mk_longt.sql to fill large-ish table
